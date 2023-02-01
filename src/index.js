3b5204905c2abc114a07d95c7d3c51e06c077fc1
Patrick Maciel <patrickmaciel.info@gmail.com>
2023-02-01 18:27:42 -0300

    added query, body and route params - enable json middleware

--- src/index.js
+++ src/index.js
@@ -2,20 +2,38 @@
 const express = require('express');
 const app = express();
 
-// create a GET route
+// middleware enable json
+app.use(express.json());
+
+/**
+ * Tipos de Parametros
+ *
+ * Route Params => params on routes, identify a resource on update or delete (edit, delete or search/find)
+ *
+ * Query Params => params on routes, used to filter, pagination, sort (search/find)
+ *
+ * Body Params => params payload, used to create or edit a resource (create or edit)
+ */
+
 app.get('/', (req, res) => {
     return res.send('Hello World Top');
 });
 
 app.get('/courses', (req, res) => {
+    const query = req.query;
+    console.log(query);
     return res.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.get('/courses/:id', (req, res) => {
-    return res.json(["Curso 10", "Curso 2", "Curso 3"]);
+    const params = req.params;
+    console.log(params);
+    return res.json([`Curso ${params.id}`, "Curso 2", "Curso 3"]);
 });
 
 app.post('/courses', (req, res) => {
+    const body = req.body;
+    console.log(body);
     return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
