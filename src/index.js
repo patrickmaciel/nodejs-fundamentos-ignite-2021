77b7cc5b67d78247a04ffce91600cabbe2a2efe9
Patrick Maciel <patrickmaciel.info@gmail.com>
2023-02-01 18:00:56 -0300

    added new endpoins to each http verb

--- src/index.js
+++ src/index.js
@@ -4,7 +4,31 @@ const app = express();
 
 // create a GET route
 app.get('/', (req, res) => {
-    res.send('Hello World Top');
+    return res.send('Hello World Top');
+});
+
+app.get('/courses', (req, res) => {
+    return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.get('/courses/:id', (req, res) => {
+    return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post('/courses', (req, res) => {
+    return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put('/courses/:id', (req, res) => {
+    return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch('/courses/:id', (req, res) => {
+    return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete('/courses/:id', (req, res) => {
+    return res.json(["Curso 6", "Curso 7", "Curso 4"]);
 });
 
 // start the server
